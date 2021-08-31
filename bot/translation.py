2df2e836273ee687ad5dedcf0be154531da9ddf0
Spiderironman1 <89696931+Spiderironman1@users.noreply.github.com>
2021-08-31 15:21:10 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,13 +4,13 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b>Hey {}!! Hi bruh 🥰</b>
 <b>I Am Just A Advance Auto Filter Bot....😉
 
 Bot gives button link to files in connected group
 
 For More Details Click Help Button Below..
-@Film_Freakers
+@saisidheswar
 </b>"""    
     
     HELP_TEXT = """
@@ -19,11 +19,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Asrith_Reddy">Asrith</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Sai_sidheswar">Sai_sidhu</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/asrith-reddy/Autofilterbot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Spiderironman1/filterautobot">Click Me</a></i>
 """
