c2786006990823bb769b014f96fe485b68e9c323
Spiderironman1 <89696931+Spiderironman1@users.noreply.github.com>
2021-09-03 06:44:59 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -13,17 +13,4 @@ For More Content join our channel 😅🥰..
 @saisidheswar
 </b>"""    
     
-    HELP_TEXT = """
-<b>Nothing Here Broo😜</b>       
-"""
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
-    
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Sai_sidheswar">Sai_sidhu</a></i></b>
-
-<b>➥ Language</b> : <code>Python3</code>
-
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
-
-<b>➥ Source Code</b> : <i><a href="https://t.me/saisidheswar">Click Me</a></i>
-"""
